84fbaa573b43a5278dfe000478755dbbf23dda63
nghuuquocthongarranger-code <nghuuquocthong.arranger@gmail.com>
2026-08-06 07:37:23 +0700

    add BaziTable above BaziHub

--- src/pages/BaziPage.tsx
+++ src/pages/BaziPage.tsx
@@ -1,11 +1,13 @@
 import { BaziHero } from "../components/sections/BaziHero";
 import { BaziTable } from "../components/sections/BaziTable";
+import { BaziHub } from "../components/sections/BaziHub";
 
 export function BaziPage() {
   return (
     <>
       <BaziHero />
       <BaziTable />
+      <BaziHub />
     </>
   );
 }
